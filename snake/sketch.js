171b6a9a5cffe380237385fb47f438be459457eb
thembu <rakhivhanithembu@gmail.com>
2021-06-30 14:05:05 +0200

    initial commit

---
+++ snake/sketch.js
@@ -0,0 +1,91 @@
+let scl = 20;
+let s;
+let score;
+let food;
+let scene;
+let currentScene;
+
+function setup() {
+  createCanvas(400, 400);
+  frameRate(10);
+  currentScene = 0;
+  s = new Snake();
+  scene = new scene1();
+  pickLocation();
+}
+
+let pickLocation  = () => {
+
+  let cols = floor(width / scl);
+  let rows = floor(height / scl);
+
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scl);
+  
+}
+
+function draw() {
+  background(255);
+
+  if (currentScene == 0) {
+    scene.display();
+  }
+
+  if (currentScene == 1) {
+    s.death();
+    s.update();
+    s.show();
+    s.points();
+     
+    
+    fill(220, 22, 27);
+    rect(food.x, food.y, scl, scl);
+  
+  }
+
+  if(s.end === true) s.Lost();
+
+  if (s.eat(food)) {
+    pickLocation();
+  }
+}
+
+
+function scene1(params) {
+
+  this.display =  () => {
+
+    background(0);
+    fill(255);
+    text("welcome to the snake game press enter to play", 50, 200);
+  };
+
+}
+
+
+
+function keyPressed() {
+  if (keyCode === 13) {
+
+ currentScene = 1
+
+  }
+
+  if(keyCode === 16) {
+  }
+  
+  if (keyCode === 38 && keyIsPressed) {
+    //UP
+    s.dir(0, -1);
+  } else if (keyCode === 40) {
+    //DOWN
+    s.dir(0, 1);
+  } else if (keyCode === 39) {
+    //LEFT
+    s.dir(1, 0);
+  } else if (keyCode === 37) {
+    //RIGHT
+    s.dir(-1, 0);
+  }
+}
+
---
+++ snake/snake.js
@@ -0,0 +1,86 @@
+function Snake(params) {
+  this.x = 0;
+  this.y = 0;
+  this.xspeed = 1;
+  this.yspeed = 0;
+  this.total = 0;
+  this.tail = [];
+  this.score = 0;
+  this.end = false;
+
+  this.show = function () {
+    fill(0, 0, 255);
+    for (let i = 0; i < this.tail.length; i++) {
+      rect(this.tail[i].x, this.tail[i].y, scl, scl);
+    }
+
+    rect(this.x, this.y, scl, scl);
+  };
+
+  this.dir = function (x, y) {
+    this.xspeed = x;
+    this.yspeed = y;
+  };
+
+  this.eat = function (pos) {
+    let d = dist(this.x, this.y, pos.x, pos.y);
+
+    if (d < 1) {
+      this.total++;
+      this.score++;
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  this.update = function () {
+    if (this.total === this.tail.length) {
+      for (let i = 0; i < this.tail.length - 1; i++) {
+        this.tail[i] = this.tail[i + 1];
+      }
+    }
+
+    this.tail[this.total - 1] = createVector(this.x, this.y);
+
+    this.x = this.x + this.xspeed * scl;
+    this.y = this.y + this.yspeed * scl;
+
+    this.x = constrain(this.x, 0, width - scl);
+    this.y = constrain(this.y, 0, height - scl);
+  };
+
+
+  this.Lost = () => {
+    background(0,244,33);
+    fill(255)
+    text(` You lost and your score is : ${this.score}` , 100,200)
+    this.x = 0;
+    this.y = 0;
+  }
+
+
+
+  this.death = function () {
+    for (let i = 0; i < this.tail.length; i++) {
+      let pos = this.tail[i];
+      let d = dist(this.x, this.y, pos.x, pos.y);
+      
+
+      if (d < 1) {
+        this.total = 0;
+        this.tail = [];
+        this.end = true;
+      }
+
+    }
+
+  };
+
+  let point = document.getElementById("scoreElem");
+
+  this.points = () => {
+    this.output = ` score :  ${this.score}`;
+    point.innerHTML = this.output;
+  };
+}
