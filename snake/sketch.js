9ffb6bf992868ee15b999359f4bc3ee68bc339b4
thembu <80485618+thembu@users.noreply.github.com>
2021-08-30 00:36:19 +0200

    Update sketch.js

--- snake/sketch.js
+++ snake/sketch.js
@@ -44,7 +44,6 @@ function draw() {
   }
 
   if(s.end === true) s.Lost();
-  if(s.Lost() == false) currentScene = 1
   if (s.eat(food)) {
     pickLocation();
   }
@@ -68,7 +67,6 @@ function keyPressed() {
   if (keyCode === 13) {
 
  currentScene = 1
- s.Lost() = false
   }
 
   if(keyCode === 16) {
